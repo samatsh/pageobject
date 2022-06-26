48230a29a0bdb51f5aea7cbac7963fbecaa02857
samatsh <samatnaprimer@yandex.ru>
2022-06-27 00:53:36 +0300

    https://stepik.org/lesson/238819/step/7

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
@@ -7,4 +8,4 @@ class MainPage(BasePage):
         login_link.click()
 
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,7 @@
 from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
@@ -12,5 +12,3 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-def should_be_login_link(self):
-    assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
